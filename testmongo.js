2b3393e07ffcd112022ee25cfa7ef4a46fd6fb1e
Cole-Bailey <91758841+Cole-Bailey@users.noreply.github.com>
2025-04-08 09:02:11 -0500

    Updated testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -1,54 +1,54 @@
 const { MongoClient } = require("mongodb");
-
-// The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://celo02:NhBf1pvorHHERD6R@cluster0.0kol5hw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// --- This is the standard stuff to get it to work on the browser
 const express = require('express');
 const app = express();
 const port = 3000;
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
+
+const uri = "mongodb+srv://celo02:NhBf1pvorHHERD6R@cluster0.0kol5hw.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=Cluster0";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// routes will go here
-
-// Default route:
-app.get('/', function(req, res) {
-  res.send('Starting... ');
+app.listen(port, () => {
+  console.log('Server started at http://localhost:' + port);
 });
 
-app.get('/say/:name', function(req, res) {
+app.get('/', (req, res) => {
+  res.send('Starting...');
+});
+
+app.get('/say/:name', (req, res) => {
   res.send('Hello ' + req.params.name + '!');
 });
 
+// changed route param from :users to :name as it was the incorrect path
+app.get('/api/mongo/:name', function(req, res) {
+  const client = new MongoClient(uri);
+  const searchKey = "{ name: '" + req.params.name + "' }";
+  console.log("Looking for: " + searchKey);
 
-// Route to access database:
-app.get('/api/mongo/:item', function(req, res) {
-const client = new MongoClient(uri);
-const searchKey = "{ email: '" + req.params.item + "' }";
-console.log("Looking for: " + searchKey);
+  async function run() {
+    try {
+      await client.connect();
+      const database = client.db('sample_mflix'); 
+      const users = database.collection('users'); 
 
-async function run() {
-  try {
-    const database = client.db('sampleDataset');
-    const parts = database.collection('userInfo');
+      const query = { name: req.params.name };
+      const user = await users.findOne(query);
+      console.log(user);
 
-    // Hardwired Query for a part that has partID '12345'
-    // const query = { partID: '12345' };
-    // But we will use the parameter provided with the route
-    const query = { partID: req.params.item };
+      if (user) {
+        res.send('Found this: ' + JSON.stringify(user));
+      } else {
+        res.status(404).send('User not found');
+      }
 
-    const part = await parts.findOne(query);
-    console.log(part);
-    res.send('Found this: ' + JSON.stringify(part));  //Use stringify to print a json
-
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    } catch (err) {
+      console.error('❌ MongoDB error:', err);
+      res.status(500).send('Internal server error');
+    } finally {
+      await client.close();
+    }
   }
-}
-run().catch(console.dir);
+
+  run().catch(console.dir);
 });
